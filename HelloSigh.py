563c4a16a943e7282411e1f673c1112feee2476f
Paavo Jokijärvi <Paavo.Jokijarvi@edu.mayk.fi>
2025-11-01 15:00:08 +0200

    Tällä kertaa se on mielivaltainen!

--- HelloSigh.py
+++
@@ -1,9 +0,0 @@
-from wsgiref.simple_server import make_server
-
-def app(environ, respond):
-	respond('200 OK', [('Content-type', 'text/plain; charset=utf-8')])
-	yield "Hello world!\n\nOK!... Let's do this one last time! (no cap fr fr this time maybe ;-)".encode('utf-8')
-
-if __name__ == '__main__':
-    with make_server("localhost", 8000, app) as server: 
-        server.serve_forever()
