3ea6fafb8e7458ed16679e1ab100b944d8062e1b
Paavo Jokijärvi <Paavo.Jokijarvi@edu.mayk.fi>
2025-11-01 11:19:32 +0200

    Palvelu, joka osaa sanoa 'Hello World' tosi hämähäkkimäisesti

--- HelloSigh.py
+++ HelloSigh.py
@@ -1,15 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
 from wsgiref.simple_server import make_server
 
 def app(environ, respond):
 	respond('200 OK', [('Content-type', 'text/plain; charset=utf-8')])
-	yield "Hello w€rld😞!".encode('utf-8')
+	yield "Hello world!\n\nOK!... Let's do this one last time! (no cap fr fr this time maybe ;-)".encode('utf-8')
 
 if __name__ == '__main__':
     with make_server("localhost", 8000, app) as server: 
